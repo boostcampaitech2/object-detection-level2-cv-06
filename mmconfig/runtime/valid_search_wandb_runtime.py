2b8d8df7cb9f36f0de2ddd6f06fbbec82d5d0801
KJ <ppskj178@gmail.com>
2021-10-04 12:58:44 +0000

    [REFACTOR] modifiy path

---
+++ mmconfig/runtime/valid_search_wandb_runtime.py
@@ -0,0 +1,15 @@
+_base_ = [
+    '/opt/ml/detection/mmdetection/configs/_base_/default_runtime.py'
+]
+
+log_config = dict(
+    interval=10,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(
+            type='WandbLoggerHook',
+            init_kwargs=dict(
+                project='valid_search',
+                name='YOUR_EXP'
+            ))
+    ])

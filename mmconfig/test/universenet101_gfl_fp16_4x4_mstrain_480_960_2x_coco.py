c9c782190885644560da4a9d800876e10fca5632
kwakjeeyoon <jeeyoon744@naver.com>
2021-10-06 15:15:51 +0000

    [FEAT] add uinversenet

---
+++ mmconfig/test/universenet101_gfl_fp16_4x4_mstrain_480_960_2x_coco.py
@@ -0,0 +1,26 @@
+_base_ = [
+    '../models/universenet101_gfl.py',
+    '../datasets/team_base_dataset.py',
+    '../schedules/schedule_adam_2x.py', '../runtime/valid_search_wandb_runtime.py'
+]
+
+model = dict(
+    bbox_head=dict(num_classes=10))
+
+data = dict(samples_per_gpu=4)
+
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(
+    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+lr_config = dict(warmup_iters=1000)
+
+fp16 = dict(loss_scale=512.)
+
+
+# --runtime--
+log_config = dict(
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='WandbLoggerHook',init_kwargs=dict(project='model',name='universenet'))
+        ]
+)

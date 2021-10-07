1421931e629c436cbf67d594e6ebee7b1ec9b28a
sne12345 <sne12345@naver.com>
2021-10-07 13:44:21 +0000

    [FIX] model test : detr & yolov3

--- mmconfig/test/detr_r50_8x2_150e_coco.py
+++ mmconfig/test/detr_r50_8x2_150e_coco.py
@@ -1,8 +1,19 @@
 _base_ = [
-    '../models/detr_r50_8x2_150e_coco_model.py', '../runtime/valid_search_wandb_runtime.py', 
+    '../models/detr_r50_8x2_150e_coco_model.py', '../runtime/model_wandb_runtime.py', 
     '../datasets/detr_dataset.py', '../schedules/schedule_adamw_detr_1x.py'
 ]
 
-# init_weights = True
+# https://download.openmmlab.com/mmdetection/v2.0/detr/detr_r50_8x2_150e_coco/detr_r50_8x2_150e_coco_20201130_194835-2c4b8974.pth
 load_from = '/opt/ml/detection/mmconfig/models/pretrained/detr_r50_8x2_150e_coco_20201130_194835-2c4b8974.pth'
 
+log_config = dict(
+    interval=100,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(
+            type='WandbLoggerHook',
+            init_kwargs=dict(
+                project='model',
+                name='detr_test'
+            ))
+    ])
--- mmconfig/test/yolov3_d53_fp16_mstrain-608_273e_coco.py
+++ mmconfig/test/yolov3_d53_fp16_mstrain-608_273e_coco.py
@@ -1,8 +1,22 @@
-_base_ = ['../models/yolov3_d53_mstrain-608_273e_coco.py', '../runtime/yolo_wandb_runtime.py', 
+_base_ = ['../models/yolov3_d53_mstrain-608_273e_coco.py', '../runtime/model_wandb_runtime.py', 
           '../datasets/yolov3_dataset.py', '../schedules/schedule_yolo_grad_clip_1x.py']
 
-# init_weights = True
+# https://download.openmmlab.com/mmdetection/v2.0/yolo/yolov3_d53_fp16_mstrain-608_273e_coco/yolov3_d53_fp16_mstrain-608_273e_coco_20210517_213542-4bc34944.pth
 load_from = load_from = '/opt/ml/detection/mmconfig/models/pretrained/yolov3_d53_fp16_mstrain-608_273e_coco_20210517_213542-4bc34944.pth'
 
 # fp16 settings
 fp16 = dict(loss_scale='dynamic')
+
+
+log_config = dict(
+    interval=100,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(
+            type='WandbLoggerHook',
+            init_kwargs=dict(
+                project='model',
+                name='yolov3_test'
+            ))
+    ])
+

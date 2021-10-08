6f68b639f36154186df8f8a7d75de6d8bdaa60b7
kwakjeeyoon <jeeyoon744@naver.com>
2021-10-08 03:46:44 +0000

    [FEAT] add universenet101_pretrained

--- mmconfig/test/universenet101_gfl_fp16_4x4_mstrain_480_960_2x_coco.py
+++ mmconfig/test/universenet101_gfl_fp16_4x4_mstrain_480_960_2x_coco.py
@@ -1,7 +1,7 @@
 _base_ = [
     '../models/universenet101_gfl.py',
     '../datasets/coco_detection_mstrain_480_960.py',
-    '../schedules/schedule_adam_2x.py', '../runtime/valid_search_wandb_runtime.py'
+    '../schedules/schedule_2x.py', '../runtime/valid_search_wandb_runtime.py'
 ]
 
 model = dict(
@@ -16,10 +16,12 @@ lr_config = dict(warmup_iters=1000)
 
 fp16 = dict(loss_scale=512.)
 
-# --runtime--
-log_config = dict(
-    hooks=[
-        dict(type='TextLoggerHook'),
-        dict(type='WandbLoggerHook',init_kwargs=dict(project='model',name='universenet'))
-        ]
-)
+# # --runtime--
+# log_config = dict(
+#     hooks=[
+#         dict(type='TextLoggerHook'),
+#         dict(type='WandbLoggerHook',init_kwargs=dict(project='model',name='universenet'))
+#         ]
+# )
+
+load_from ='https://github.com/shinya7y/UniverseNet/releases/download/20.07/universenet101_gfl_fp16_4x4_mstrain_480_960_2x_coco_20200716_epoch_24-1b9a1241.pth'

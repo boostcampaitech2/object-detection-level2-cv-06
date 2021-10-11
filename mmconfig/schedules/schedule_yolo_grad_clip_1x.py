351b15796c745919f1ffa55518a907b2746e6da7
송sne12345 <sne12345@naver.com>
2021-10-11 06:10:41 +0000

    add yolov3 code

--- mmconfig/schedules/schedule_yolo_grad_clip_1x.py
+++ mmconfig/schedules/schedule_yolo_grad_clip_1x.py
@@ -8,6 +8,6 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=2000,  # same as burn-in in darknet
     warmup_ratio=0.1,
-    step=[8, 11])
+    step=[30, 40])
 # runtime settings
-runner = dict(type='EpochBasedRunner', max_epochs=12)
+runner = dict(type='EpochBasedRunner', max_epochs=50)
--- mmconfig/test/yolov3_d53_fp16_mstrain-608_273e_coco.py
+++ mmconfig/test/yolov3_d53_fp16_mstrain-608_273e_coco.py
@@ -2,7 +2,7 @@ _base_ = ['../models/yolov3_d53_mstrain-608_273e_coco.py', '../runtime/model_wan
           '../datasets/yolov3_dataset.py', '../schedules/schedule_yolo_grad_clip_1x.py']
 
 # https://download.openmmlab.com/mmdetection/v2.0/yolo/yolov3_d53_fp16_mstrain-608_273e_coco/yolov3_d53_fp16_mstrain-608_273e_coco_20210517_213542-4bc34944.pth
-load_from = load_from = '/opt/ml/detection/mmconfig/models/pretrained/yolov3_d53_fp16_mstrain-608_273e_coco_20210517_213542-4bc34944.pth'
+load_from = '/opt/ml/detection/mmconfig/models/pretrained/yolov3_d53_fp16_mstrain-608_273e_coco_20210517_213542-4bc34944.pth'
 
 # fp16 settings
 fp16 = dict(loss_scale='dynamic')
